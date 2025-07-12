fc95ce8110dc0a251640f880c0c8ed05ad8d8114
akshayraj-1905 <241310001@nitdelhi.ac.in>
2025-07-12 16:51:48 +0530

    solved Find Missing And Repeated Numbers

---
+++ 2965-Find-missing-and-repeated-value/findMissingNandRepeatedValue.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+        int n = grid.size();
+        vector<int> freq(n*n + 1,0);
+        vector<int>ans(2);
+
+        for(int row = 0 ; row < n ; row++){
+            for(int col = 0 ; col < n ; col++){
+                int idx = grid[row][col];
+                freq[idx]++; // storing frequency of each element
+            }
+        }
+
+        for(int i = 1 ; i < freq.size() ; i++){
+            if(freq[i] == 0){ // frequency = 0 means missing
+                ans[1] = i;
+            }
+            else if (freq[i] > 1){// frequency = 2 means repeating
+                ans[0] = i;
+            }
+        }
+
+        return ans;
+    }
+};
