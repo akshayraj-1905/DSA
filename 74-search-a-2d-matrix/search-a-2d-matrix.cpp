22c200c37470e680bc5626870359b4d79c39daf6
Akshay Raj <241310001@nitdelhi.ac.in>
2025-07-16 15:09:07 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (41.92%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,44 +1,31 @@
 class Solution {
-private:
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
 
-    bool binarysearch(vector<vector<int>>& matrix, int sr, int ec, int target){
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+
         int start = 0;
-        int end = ec;
-        int mid = start + (end - start)/2;
+        int end = rows * cols - 1;
+        int mid = start + (end - start) / 2;
 
-        while(start <= end){
-            if(matrix[sr][mid] == target){
+        while (start <= end) {
+
+            int row = mid / cols;
+            int col = mid % cols;
+
+            if (matrix[row][col] == target) {
                 return true;
             }
-
-            if(matrix[sr][mid] < target){
+            if (matrix[row][col] < target) {
                 start = mid + 1;
-            }
-            else{
+            } else {
                 end = mid - 1;
             }
 
-            mid = start + (end - start)/2;
+            mid = start + (end - start) / 2;
         }
 
         return false;
-    }
-
-public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target){ 
-        int ec = matrix[0].size() - 1; //ending column
-
-        if(matrix.size() == 1){
-            return binarysearch(matrix,0,ec,target);
-        }
-
-        for(int sr = 0 ; sr < matrix.size() ; sr++){ //searching row
-            if(target >= matrix[sr][0] && target <= matrix[sr][ec]){
-                return binarysearch(matrix,sr,ec,target);
-            }
-        }
-
-        return false;
-
     }
 };
